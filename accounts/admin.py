6bee53252dbd2f6ce04c06a7677fcc8360734b1d
tabassom-entezami <tabassom.entezami@gmail.com>
2021-12-29 10:27:31 +0330

    adding template

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,7 +12,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email']
     list_display_links = ['email']
     search_fields = ['email']
-
+    
     def get_queryset(self, request):
             return Customer.objects.filter(is_staff=False)
 
@@ -21,22 +21,25 @@ class CustomerAdmin(admin.ModelAdmin):
 class CustomerAdressAdmin(admin.ModelAdmin):
     list_display = ['city', 'street', 'plaque']
     search_fields = ['city']
+    empty_value_display = "---"
 
 @admin.register(Manager)
 class managerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email']
     search_fields = ['email']
+    empty_value_display = "---"
     def get_queryset(self, request):
         return Manager.objects.filter(is_staff=True, is_superuser=False)
 
 @admin.register(Admin)
 class AdminProxyAdmin(admin.ModelAdmin):
-      list_display = ['id', 'username', 'email']
-      list_display_links = ['username']
-      list_editable = ['email']
-      search_fields = ['username', 'email']
-
-      def get_queryset(self, request):
+    list_display = ['id', 'username', 'email']
+    list_display_links = ['username']
+    list_editable = ['email']
+    search_fields = ['username', 'email']
+    empty_value_display = "---"
+    
+    def get_queryset(self, request):
         return Admin.objects.filter(is_superuser=True)
 
 # Register your models here.
