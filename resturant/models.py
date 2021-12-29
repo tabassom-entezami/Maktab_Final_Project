7188c6da45f577ece23ac890bdf5202d87bf72f8
tabassom-entezami <tabassom.entezami@gmail.com>
2021-12-29 11:05:01 +0330

    adding base(header and footer) and add foodmenu model

--- resturant/models.py
+++ resturant/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from django.db.models.deletion import CASCADE
 from accounts.models import Manager,CustomerAdress
 
     
@@ -15,23 +16,23 @@ class Beranch(Resturant):
         (0,"Open"),
         (1,"Close"),
     }
-    manager_id=models.OneToOneField(Manager,on_delete=models.CASCADE)
-    category_id=models.OneToOneField("FoodCategory",on_delete=models.CASCADE)
+    manager_id=models.OneToOneField(Manager,on_delete=models.CASCADE,verbose_name="which_manager",related_name='maneger_id')
+    category_id=models.OneToOneField("FoodCategory",on_delete=models.CASCADE,verbose_name="which_category",related_name="category_id")
     address=models.CharField(max_length=50)
     city=models.CharField(max_length=30)
     discreption=models.TextField(max_length=200)
     is_open= models.BooleanField(choices=OPEN_STATUS, verbose_name='IsOpen', default=False)
     create_date=models.DateTimeField(auto_now_add=True)
-    order_id=models.ForeignKey("Order",on_delete=models.CASCADE,null=True)
+    order_id=models.ForeignKey("Order",on_delete=models.CASCADE,null=True,verbose_name="which_order",related_name='order_id')
 
     def __str__(self) :
         return self.name
 
 class Menu(models.Model):
     branch_id=models.OneToOneField(Beranch,on_delete=models.CASCADE)
-    food_id=models.ManyToManyField("Food")
-    price=models.IntegerField(null=False, blank=False)
-    quantity=models.IntegerField(null=False, blank=False)
+    foodmenu_id=models.ManyToManyField("FoodMenu",related_name='foodmenu_id')
+    # price=models.IntegerField(null=False, blank=False)
+    # quantity=models.IntegerField(null=False, blank=False)
 
     def __str__(self) -> str:
         return f'{self.branch_id}'
@@ -48,6 +49,15 @@ class Food(models.Model):
     def __str__(self) :
         return self.name
 
+class FoodMenu(models.Model):
+    food_id = models.ForeignKey("Food", on_delete=models.CASCADE ,verbose_name="which_food" )
+    menu_id = models.ForeignKey("Menu",on_delete=models.CASCADE,verbose_name="which_menu")
+    price = models.IntegerField()
+    number = models.IntegerField()
+
+    def __str__(self) :
+        return self.food_id + "_" + self.menu_id
+
 class FoodCategory(models.Model):
     type=models.CharField(max_length=30)
 
@@ -56,7 +66,7 @@ class FoodCategory(models.Model):
 
 class Meal(models.Model):
     name=models.CharField(max_length=30)
-    food_id=models.ManyToManyField(Food)
+    food_id=models.ManyToManyField(Food,related_name='food_id')
 
     def __str__(self) :
         return self.name
@@ -69,7 +79,8 @@ class Order(models.Model):
         ('Delivery', 'Delivery'),
         
     )
-    order_count=models.IntegerField()
+    # order_count=models.IntegerField()
+    food_menu_id = models.ManyToManyField("Foodmenu")
     total_price=models.IntegerField()
     delivery_time=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=20, choices=ORDER_STATUS, default='Record')
