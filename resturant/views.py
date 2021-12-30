a1c4e511886ff8be0aa636a2e7c040d5f2d6dd9a
tabassom-entezami <tabassom.entezami@gmail.com>
2021-12-30 15:53:06 +0330

    make design home

--- resturant/views.py
+++ resturant/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
+from django.db import models
+from django.http import request
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, TemplateView, CreateView
+from .models import *
+from accounts.models import *
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 def home_page(re):
+
     return render(re,"Home.html")
 # Create your views here.
